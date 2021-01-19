5a368ed156125f1f0668b2100a2fe04941141231
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-01-19 17:10:34 -0300

    FactoringToHooksComponents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Candidates from './components/Candidates';
 import Header from './components/Header';
 import Spinner from './components/Spinner';
 
-export default class App extends Component {
-  constructor() {
-    super();
+export default function App() {
+  const [candidates, setCadidates] = useState([]);
 
-    this.state = {
-      candidates: [],
-    };
-    this.interval = null;
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => {
+  useEffect(() => {
+    const interval = setInterval(() => {
       fetch('http://localhost:8080/votes')
         .then((res) => {
           return res.json();
         })
         .then((json) => {
-          this.setState({
-            candidates: json.candidates,
-          });
+          setCadidates(json.candidates);
         });
     }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [candidates]);
+
+  if (candidates.length === 0) {
+    return <Spinner description="Carregando" />;
   }
 
-  render() {
-    const { candidates } = this.state;
-
-    if (candidates.length === 0) {
-      return <Spinner description="Carregando" />;
-    }
-
-    return (
-      <div className="container">
-        <Header>Votação</Header>
-        <Candidates candidates={candidates} />
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <Header>Votação</Header>
+      <Candidates candidates={candidates} />
+    </div>
+  );
 }
